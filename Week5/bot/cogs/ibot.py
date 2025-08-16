b6c8e05c38fbb1b3feee113e0d0b506afb0dc32a
weave-and-grow <davidbenjamin2816@gmail.com>
2025-08-16 08:12:47 -0500

    Progress Week 5

--- Week5/bot/cogs/ibot.py
+++ Week5/bot/cogs/ibot.py
@@ -383,7 +383,7 @@ class GenAIBot(commands.Cog):
     @nextcord.slash_command(
         guild_ids=[config['guild_id']],
         description="Execute Command")
-    async def raginator(
+    async def Bottastic(
             self,
             interaction: nextcord.Interaction,
             user_message: str
